48c5ec46c1027d048d6ca0f394c3470ff3ea03f3
14021973 <61793315+14021973@users.noreply.github.com>
2020-03-17 14:14:34 +0100

    zmiana wersji

--- src/pl/biblioteka/app/LibraryApp.java
+++ src/pl/biblioteka/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.biblioteka.app;
 
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName="Biblioteka chybAAA ver 1.1";
+        final String appName="Biblioteka chyba ver 1.11";
         System.out.println(appName);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
