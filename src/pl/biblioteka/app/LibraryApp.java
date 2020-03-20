b0d1971ed618d8e119676bf4c43bd180946c3018
kzaw <kzawadzki@wp.pl>
2020-03-20 13:21:03 +0100

    zmiana wersji na 1.17

--- src/pl/biblioteka/app/LibraryApp.java
+++ src/pl/biblioteka/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.biblioteka.app;
 
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName="Biblioteka chyba ver 1.16";
+        final String appName="Biblioteka chyba ver 1.17";
         System.out.println(appName);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
